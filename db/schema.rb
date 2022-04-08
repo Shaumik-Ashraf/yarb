756b77c51311f8b4eb9df33e8e3171267eb32635
Shaumik-Ashraf <a.shaumik@gmail.com>
2022-04-08 13:49:27 -0400

    generate scaffold Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_08_174206) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_08_174603) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -49,9 +49,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_174206) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.boolean "permanent"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.boolean "admin"
+    t.string "name", null: false
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_users_on_name", unique: true
@@ -59,4 +68,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_174206) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
